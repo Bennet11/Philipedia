ca9cc259957d9c1089361c4fa2be12aac50d43f6
Bennet11 <bennetveloya@gmail.com>
2016-07-18 01:29:28 -0700

    markdown successful

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'devise'
 gem 'pundit'
 gem 'stripe'
 gem 'faker'
+gem 'redcarpet'
 
 group :development, :test do
   gem 'byebug'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def markdown_to_html(markdown)
+    render_options = { prettify: true }
+    extensions = { autolink: true, highlight: true }
+    renderer = Redcarpet::Render::HTML.new(render_options)
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
 end
